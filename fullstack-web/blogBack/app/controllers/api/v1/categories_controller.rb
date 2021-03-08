7b8c58cf2c4f571e53da73db6bb2b671897e4589
JoaoGabriel0511 <joaogabriel2010@hotmail.com>
2021-03-07 23:03:45 -0300

    feat(Category): create show method in CategoriesController

--- fullstack-web/blogBack/app/controllers/api/v1/categories_controller.rb
+++ fullstack-web/blogBack/app/controllers/api/v1/categories_controller.rb
@@ -2,10 +2,25 @@
 module Api
   module V1
     class CategoriesController < ApplicationController
+      before_action :set_category, only: [:show, :update, :destroy]
 
       def index
         categories = Category.order('created_at DESC');
         render json: {status: 'SUCCESS', message:'Categorias carregadas', data:categories},status: :ok
+      end
+
+      def show
+        render json: {status: 'SUCCESS', message:'Categoria carregada', data:@category }
+      end
+
+      private
+
+      def category_params
+        params.require(:category).permit(:name, :description)
+      end
+
+      def set_category
+        @category = Category.find(params[:id])
       end
 
     end
