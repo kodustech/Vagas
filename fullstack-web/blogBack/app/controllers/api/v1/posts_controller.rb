ef60f48dfcd95d7c2f00d728f0ba8da496db503b
JoaoGabriel0511 <joaogabriel2010@hotmail.com>
2021-03-08 18:42:21 -0300

    feat(Post): add show method to PostController

--- fullstack-web/blogBack/app/controllers/api/v1/posts_controller.rb
+++ fullstack-web/blogBack/app/controllers/api/v1/posts_controller.rb
@@ -2,9 +2,21 @@ module Api
   module V1
     class PostsController < ApplicationController
 
+      before_action :set_post, only: [:show, :update, :destroy]
+
       def index
         posts = Post.order('created_at DESC');
         render json: {status: 'SUCCESS', message:'Posts carregados', data:posts},status: :ok
+      end
+
+      def show
+        render json: {status: 'SUCCESS', message:'Post carregado', data: @post},status: :ok
+      end
+
+      private
+
+      def set_post
+        @post = Post.find(params[:id])
       end
 
     end
